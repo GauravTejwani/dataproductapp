c917f2e2b644e9183d857ce1a3b44e5441b47482
GauravTejwani <gaurav.tejwani@gmail.com>
2014-10-26 12:01:52 +0530

    Update server.R

--- server.R
+++ server.R
@@ -13,5 +13,13 @@ shinyServer(
     output$inputValue1 <- renderPrint({if(input$radio==0) "Male" else "Female"})
     output$inputValue2 <- renderPrint({input$height})
     output$prediction <- renderPrint({sonheight(input$height*(1+0.08*as.numeric(input$radio)))})
+    
+    output$newHist <- renderPlot({
+      plot(galton$parent,galton$child, xlab='parent height', ylab='child height',col='blue')
+      abline(model,lwd=2)
+      x <- input$height*(1+0.08*as.numeric(input$radio))
+      y <- constant + beta*x
+      points(c(x,x), c(y, y),col="red",lwd=5)
+    })
   }
 )
